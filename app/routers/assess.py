26cb9b5488dbb166bf23454024e4d19f4212417e
Andres Montero <and.montero.martin@gmail.com>
2026-02-20 12:56:42 +0100

    Added short id compliance to POST assess/test/ endpoint

--- app/routers/assess.py
+++ app/routers/assess.py
@@ -8,11 +8,13 @@ router = APIRouter(prefix="/assess", tags=["api-controller"])
 
 @router.post("/test/{test_identifier:path}")
 async def post_test_assessment(test_identifier: str = Path(..., description="Identifier of the test to run"), body: ResourceAssessmentRequest = ...):
-    if test_identifier not in utils.TEST_IDENTIFIERS:
+    if test_identifier not in utils.TEST_IDENTIFIERS or test_identifier not in utils.TEST_SHORTIDS:
         raise HTTPException(status_code=404, detail="Test not found")
+    else:
+        if test_identifier in utils.TEST_IDENTIFIERS:
+            short_id = test_identifier.rstrip("/").rsplit("/", 1)[-1]
 
     try:
-        short_id = test_identifier.rstrip("/").rsplit("/", 1)[-1]
         return await docker_executor.run_assessment(body.resource_identifier, short_id)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
